fee30e0430fe793039e42a62a1dfb40bf4a2dcef
Berrucaz <sev.berrucaz@gmail.com>
2022-03-07 12:18:06 +0100

    Ajout contact.html

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template # import de l’objet Flask
 
-
-
 app = Flask(__name__) # instanciation application
 
 @app.route("/")  # association d’une route (URL) avec la fonction suivante
 def home():
     return render_template('/page1.html')   # on renvoie une chaîne de caractères
+
+@app.route("/contact")  # association d’une route (URL) avec la fonction suivante
+def contact():
+    return render_template('contact.html')   # on renvoie une chaîne de caractères
 
 app.run() # démarrage de l’appli

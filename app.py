514a836d6d37dea38acae304374b9003d1ba8598
Berrucaz <sev.berrucaz@gmail.com>
2022-03-07 15:29:17 +0100

    result merge conflict

--- app.py
+++ app.py
@@ -11,22 +11,7 @@ def home():
 def contact():
     return render_template('/contact.html')   # on renvoie une chaîne de caractères
 
-<<<<<<< HEAD
 @app.route("/Thanks")  # association d’une route (URL) avec la fonction suivante
 def thanks():
     return render_template('Thanks.html')   # on renvoie une chaîne de caractères
 app.run(debug = True) # démarrage de l’appli
-=======
-@app.route("/nom")
-def nom(text='pas de résumé'):
-    sentiment = "négatif"
-    if sentiment == "positif":
-        text="ce résumé est positif"
-    else :
-        text= "ce résumé est négatif"
-    return render_template('/nom.html',text=text) 
-
-app.run(debug=True) # démarrage de l’appli
- 
- 
->>>>>>> 76a4bd1fc8238b469fa7869f2f32b250be2fec02

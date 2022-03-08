43f3fdc7a11538351128235045c296221721242d
AminaBENDJEBBOUR <bendjebbour.amina@yahoo.fr>
2022-03-08 15:15:32 +0100

    modif EXECUTE THE SQL QUERY

--- code_connection_to_DBserver.py
+++ code_connection_to_DBserver.py
@@ -1,4 +1,6 @@
 
+
+#the following code can be used to create a connection to the database server:
 import psycopg2
   
 def get_connection():
@@ -19,3 +21,27 @@ if conn:
     print("Connection to the PostgreSQL established successfully.")
 else:
     print("Connection to the PostgreSQL encountered and error.")
+conn.autocommit=True
+
+# This program uses fetchall()
+
+  
+  
+# CREATE A CURSOR USING THE CONNECTION OBJECT
+curr = conn.cursor()
+  
+# EXECUTE THE SQL QUERY
+curr.execute(" CREATE TABLE IF NOT EXISTS IDENTIFICATION (msg_id INT PRIMARY KEY NOT NULL, name VARCHAR(100), message VARCHAR(255) );")
+curr.execute(" INSERT INTO IDENTIFICATION (msg_id, name,message) VALUES (1,'AMINA','Bonjour');")
+#curr.execute("SELECT * FROM IDENTIFICATION ;")
+
+# FETCH ALL THE ROWS FROM THE CURSOR
+data = curr.fetchall()
+
+  
+# PRINT THE RECORDS
+for row in data:
+    print(row)
+  
+# CLOSE THE CONNECTION
+conn.close()
